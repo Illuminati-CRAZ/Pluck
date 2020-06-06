2df6034021df7b118598a87bc697b6e35546c201
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2020-06-05 18:40:54 -0700

    Update plugin.lua
    Make object length depend on bpm at object start time rather than at editor time

--- Pluck/plugin.lua
+++ Pluck/plugin.lua
@@ -3,26 +3,20 @@ function draw()
 
     local snap = state.GetValue("snap") or 4
 
-    local timingPoint = state.CurrentTimingPoint
-    local bpm = timingPoint.Bpm
-    local mspb = 60000 / bpm
-    local length = mspb / snap
-
     _, snap = imgui.InputInt("Snap", snap)
 
     if imgui.Button("Pluck") then
         for _, object in pairs(state.SelectedHitObjects) do
             if object.EndTime == 0 then --if object is a note, replace with LN
+                local timingPoint = map.GetTimingPointAt(object.StartTime)
+                local bpm = timingPoint.Bpm
+                local mspb = 60000 / bpm
+                local length = mspb / snap
                 actions.PlaceHitObject(utils.CreateHitObject(object.StartTime, object.Lane, object.StartTime+length, object.HitSound))
                 actions.RemoveHitObject(object)
             end
         end
     end
-
-    imgui.Text("")
-    imgui.Text("BPM: " .. bpm)
-    imgui.Text("ms per Beat: " .. mspb)
-    imgui.Text("LN Length: " .. length)
 
     state.SetValue("snap", snap)
 
